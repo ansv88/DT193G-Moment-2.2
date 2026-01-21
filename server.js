81127133f94047c0e490c7115d5796dc378f51ad
Annelie Svensson <annelievs@hotmail.com>
2026-01-21 23:37:37 +0100

    Justerat serverinställningar för att kunna publicera på Render

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ const init = async () => {
 
     // Skapar Hapi‑server
     const server = Hapi.server({
-        port: 5000,
-        host: 'localhost'
+        port: process.env.PORT || 5000,
+        host: '0.0.0.0'
     });
 
     // Anslut till MongoDB
